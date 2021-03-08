e1fae4b28278d4af14ee224f8c9dcd25854b9954
Scott Overholser <eigenholser@users.noreply.github.com>
2021-03-07 23:02:54 -0700

    Prototype configuration file

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,23 @@
+package flac2mp3
+
+import com.typesafe.config.ConfigFactory
+import io.github.config4k.extract
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val configFile = System.getenv("HOME") + "/flac2mp3.properties"
+    println("Reading from $configFile")
+
+    val config = ConfigFactory.parseFile(File(configFile))
+    val mp3Root = config.extract<String>("mp3_root")
+    println(mp3Root)
+    val flacRoot = config.extract<String>("flac_root")
+    println(flacRoot)
+    val albumArtFile = config.extract<String>("album_art.name")
+    println(albumArtFile)
+    val coverResolution = config.extract<Int>("album_art.resolution.cover")
+    println(coverResolution)
+    val thumbnailResolution = config.extract<Int>("album_art.resolution.thumb")
+    println(thumbnailResolution)
 }
+
