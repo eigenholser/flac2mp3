c3464a26761ae154438572a877d3d3389b32b3e1
Ry Overholser <overry@isu.edu>
2021-03-14 15:01:07 -0600

    Update Tags.kt with default values for missing year and genre tags, to implement MD5 hashing for tracks with no included CDDB, and update FlacDatabase.kt to accomadate MD5 hashes.

--- src/main/kotlin/Tags.kt
+++ src/main/kotlin/Tags.kt
@@ -5,11 +5,20 @@ import org.jaudiotagger.tag.FieldKey
 import org.jaudiotagger.tag.id3.ID3v23Tag
 import org.jaudiotagger.tag.images.StandardArtwork
 import java.io.File
+import java.lang.IllegalArgumentException
 import java.util.logging.Logger
+import java.math.BigInteger
+import java.security.MessageDigest
 
-data class FlacTags(val artist: String, val album: String, val title: String,
+data class FlacTags(
+    val artist: String, val album: String, val title: String,
     val year: String, val genre: String, val track: String, val cddb: String
 )
+
+fun md5(input:String): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest(input.toByteArray())).toString(16).padStart(32, '0')
+}
 
 object Tag {
     fun readFlacTags(flacFile: String): FlacTags {
@@ -19,10 +28,26 @@ object Tag {
         val artist = tag.getFirst(FieldKey.ARTIST)
         val album = tag.getFirst(FieldKey.ALBUM)
         val title = tag.getFirst(FieldKey.TITLE)
-        val year = tag.getFirst(FieldKey.YEAR)
-        val genre = tag.getFirst(FieldKey.GENRE)
+        val year = if (tag.getFirst(FieldKey.YEAR) == "") {
+            "0000"
+        } else {
+            tag.getFirst(FieldKey.YEAR)
+        }
+        val genre = if (tag.getFirst(FieldKey.GENRE) == "") {
+            "None"
+        } else {
+            tag.getFirst(FieldKey.GENRE)
+        }
         val track = tag.getFirst(FieldKey.TRACK)
-        val cddb = tag.getFirst("CDDB")
+        val cddb = if (tag.getFirst("CDDB") == "") {
+            if (tag.getFirst("MD5 SIGNATURE") == "") {
+                md5(title)
+            } else {
+                tag.getFirst("MD5 SIGNATURE")
+            }
+        } else {
+            tag.getFirst("CDDB")
+        }
         val tags = FlacTags(artist, album, title, year, genre, track, cddb)
 
         return tags
@@ -35,12 +60,14 @@ object Tag {
         try {
             val albumArt = StandardArtwork.createArtworkFromFile(File("$albumArtFile/cover.jpg"))
             tag.addField(albumArt)
-        }catch (e: Throwable){
-            Logger.getLogger("Tags Warning: ").warning("Bad file path: File ".plus("$albumArtFile/cover.jpg").plus(" not found"))
+        } catch (e: Throwable) {
+            Logger.getLogger("Tags Warning: ")
+                .warning("Bad file path: File ".plus("$albumArtFile/cover.jpg").plus(" not found"))
         }
         tag.setField(FieldKey.ARTIST, flacTags.artist)
         tag.setField(FieldKey.ALBUM, flacTags.album)
         tag.setField(FieldKey.TITLE, flacTags.title)
+        println(flacTags)
         tag.setField(FieldKey.YEAR, flacTags.year)
         tag.setField(FieldKey.GENRE, flacTags.genre)
         tag.setField(FieldKey.TRACK, flacTags.track)
