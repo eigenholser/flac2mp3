1e2414cfed82bf408c755ef2aa720b67c7a5adff
Ry Overholser <overry@isu.edu>
2021-07-25 13:19:20 -0600

    Practice branch. Convert main method to use if statements

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,6 @@
 package com.eigenholser.flac2mp3
 
-import com.eigenholser.flac2mp3.rules.*
-import com.eigenholser.flac2mp3.states.AlbumState.albumStateMachine
 import com.eigenholser.flac2mp3.states.AlbumState.state
-import com.eigenholser.flac2mp3.states.AlbumStates
-import com.eigenholser.flac2mp3.states.ExistingAlbumEvent
-import org.jeasy.rules.api.*
 import org.jeasy.rules.core.DefaultRulesEngine
 import java.io.File
 import java.nio.file.Files
@@ -24,67 +19,119 @@ fun main(args: Array<String>) {
     // FlacDatabase.createDatabase()
 
     val rulesEngine = DefaultRulesEngine()
+    var currentFlacAlbum = ""
+    var prevFlacAlbum = ""
+    var currentMP3AlbumPath = ""
+    var prevMP3AlbumPath = ""
 
     File(Config.flacRoot)
         .walk()
-        .filter {it.extension == "flac"}
+        .filter { it.extension == "flac" }
         .map { file ->
             val flacfile = file.absolutePath
             val fsize = Files.getAttribute(file.toPath(), "size") as Long
             val mtime = Files.getAttribute(file.toPath(), "lastModifiedTime") as FileTime
             convertRow(flacfile, fsize, mtime.toMillis())
         }
-        .filter (::isThisTrackStale)
+        .filter(::isThisTrackStale)
         .forEach {
-            logger.info(it.toString())
 
-            val rules = Rules(NewAlbum(albumStateMachine))
-            val facts = Facts()
-            facts.add(Fact(AlbumFact.ALBUM_STATE.toString(), albumStateMachine))
-            facts.add(Fact(AlbumFact.CURRENT_ALBUM.toString(), it.currentAlbum))
-            facts.add(Fact(AlbumFact.NEXT_ALBUM.toString(), state.nextAlbum))
-            rulesEngine.fire(rules, facts)
-
-            val albumArtFacts = Facts()
-            albumArtFacts.add(Fact(AlbumArtFacts.TRACK_DATA.toString(), it))
-            albumArtFacts.add(Fact(AlbumArtFacts.ALBUM_STATE.toString(), albumStateMachine))
-            val parameters = RulesEngineParameters()
-                .skipOnFirstAppliedRule(true)
-
-            when (AlbumStates.valueOf(albumStateMachine.currentState.name)) {
-                AlbumStates.NEW_ALBUM -> {
-                    deleteMp3CoverArt(state.prevMp3AlbumPath)
-
-                    state.nextAlbum = it.currentAlbum
-                    state.prevMp3AlbumPath = it.mp3AlbumPathAbsolute
-                    Files.createDirectories(it.mp3AlbumPathAbsolute)
-
-                    val albumArtRulesEngine = DefaultRulesEngine(parameters)
-                    val albumArtRules = Rules(ArtNewMp3(), ArtUpdateIDv3())
-                    albumArtRulesEngine.fire(albumArtRules, albumArtFacts)
-
-                    albumStateMachine.fire(ExistingAlbumEvent())
+            if (currentFlacAlbum.compareTo(it.flacAlbumPathAbsolute.toString()) != 0) {
+                if (File("$prevMP3AlbumPath").toPath().resolve(Config.coverArtFile).exists()) {
+                    deleteMp3CoverArt(File(prevMP3AlbumPath).toPath())
                 }
-                AlbumStates.EXISTING_ALBUM -> {
-                    val albumArtRulesEngine = DefaultRulesEngine(parameters)
-                    val albumArtRules = Rules(ArtNewMp3(), ArtUpdateIDv3())
-                    albumArtRulesEngine.fire(albumArtRules, albumArtFacts)
+                logger.info("We've switched albums")
+                prevFlacAlbum = currentFlacAlbum
+                currentFlacAlbum = it.flacAlbumPathAbsolute.toString()
 
-                    if (!isTrackCurrent(it)) {
-                        LameFlac2Mp3.flac2mp3(
-                            it.flacFileAbsolute.toString(),
-                            it.mp3FileAbsolute.toString(),
-                            it.mp3AlbumPathAbsolute
-                        )
-
-                        val flacTags = Tag.readFlacTags(it.flacFileAbsolute.toString())
-                        Tag.writeMp3Tags(it.mp3FileAbsolute.toString(), it.mp3AlbumPathAbsolute.toString(), flacTags)
-                    }
+                prevMP3AlbumPath = currentMP3AlbumPath
+                currentMP3AlbumPath = it.mp3AlbumPathAbsolute.toString()
+                if (!it.mp3AlbumPathAbsolute.exists()) {
+                    Files.createDirectories(it.mp3AlbumPathAbsolute)
+                }
+                logger.info("Is PNG album art present: ${albumArtPNGExists(it)}")
+                if (albumArtPNGExists(it)) {
+                    ImageScaler.scaleImage(
+                        it.flacAlbumPathAbsolute.toString(),
+                        it.mp3AlbumPathAbsolute.toString(),
+                        DestType.COVER
+                    )
                 }
             }
+//            logger.info("Previous album: $prevFlacAlbum")
+//            logger.info("Current album: $currentFlacAlbum")
+//            logger.info("Previous MP3 album: $prevMP3AlbumPath")
+            if (!mp3FileExists(it) || !isTrackCurrent(it)) {
+                LameFlac2Mp3.flac2mp3(
+                    it.flacFileAbsolute.toString(),
+                    it.mp3FileAbsolute.toString(),
+                    it.mp3AlbumPathAbsolute
+                )
+
+                val flacTags = Tag.readFlacTags(it.flacFileAbsolute.toString())
+                Tag.writeMp3Tags(it.mp3FileAbsolute.toString(), it.mp3AlbumPathAbsolute.toString(), flacTags)
+            } else {
+                if (isAlbumArtUpdated(it)) {
+                    Tag.updateAlbumArtField(
+                        it.mp3FileAbsolute.toString(),
+                        it.mp3AlbumPathAbsolute.toString()
+                    )
+//                    logger.info("Passing ${it.mp3FileAbsolute} and ${it.mp3AlbumPathAbsolute} to updateAlbumArtField")
+                }
+            }
+
+
+//            logger.info(it.toString())
+//
+//            val rules = Rules(NewAlbum(albumStateMachine))
+//            val facts = Facts()
+//            facts.add(Fact(AlbumFact.ALBUM_STATE.toString(), albumStateMachine))
+//            facts.add(Fact(AlbumFact.CURRENT_ALBUM.toString(), it.currentAlbum))
+//            facts.add(Fact(AlbumFact.NEXT_ALBUM.toString(), state.nextAlbum))
+//            rulesEngine.fire(rules, facts)
+//
+//            val albumArtFacts = Facts()
+//            albumArtFacts.add(Fact(AlbumArtFacts.TRACK_DATA.toString(), it))
+//            albumArtFacts.add(Fact(AlbumArtFacts.ALBUM_STATE.toString(), albumStateMachine))
+//            val parameters = RulesEngineParameters()
+//                .skipOnFirstAppliedRule(true)
+//
+//            when (AlbumStates.valueOf(albumStateMachine.currentState.name)) {
+//                AlbumStates.NEW_ALBUM -> {
+//                    deleteMp3CoverArt(state.prevMp3AlbumPath)
+//
+//                    state.nextAlbum = it.currentAlbum
+//                    state.prevMp3AlbumPath = it.mp3AlbumPathAbsolute
+//                    Files.createDirectories(it.mp3AlbumPathAbsolute)
+//
+//                    val albumArtRulesEngine = DefaultRulesEngine(parameters)
+//                    val albumArtRules = Rules(ArtNewMp3(), ArtUpdateIDv3())
+//                    albumArtRulesEngine.fire(albumArtRules, albumArtFacts)
+//
+//                    albumStateMachine.fire(ExistingAlbumEvent())
+//                }
+//                AlbumStates.EXISTING_ALBUM -> {
+//                    val albumArtRulesEngine = DefaultRulesEngine(parameters)
+//                    val albumArtRules = Rules(ArtNewMp3(), ArtUpdateIDv3())
+//                    albumArtRulesEngine.fire(albumArtRules, albumArtFacts)
+//
+//                    if (!isTrackCurrent(it)) {
+//                        LameFlac2Mp3.flac2mp3(
+//                            it.flacFileAbsolute.toString(),
+//                            it.mp3FileAbsolute.toString(),
+//                            it.mp3AlbumPathAbsolute
+//                        )
+//
+//                        val flacTags = Tag.readFlacTags(it.flacFileAbsolute.toString())
+//                        Tag.writeMp3Tags(it.mp3FileAbsolute.toString(), it.mp3AlbumPathAbsolute.toString(), flacTags)
+//                    }
+//                }
+//            }
         }
     // Delete the last album art
-    deleteMp3CoverArt(state.prevMp3AlbumPath)
+        if (File(currentMP3AlbumPath).toPath().resolve(Config.coverArtFile).exists()) {
+            deleteMp3CoverArt(File(currentMP3AlbumPath).toPath())
+        }
 }
 
 data class TrackData(
@@ -121,8 +168,10 @@ fun isTrackCurrent(trackData: TrackData): Boolean {
 fun mp3FileExists(trackData: TrackData): Boolean {
     return trackData.mp3FileAbsolute.exists()
 }
+
 @ExperimentalPathApi
-fun albumArtPNGExists(trackData: TrackData): Boolean = trackData.flacAlbumPathAbsolute.toPath().resolve(Config.albumArtFile).exists()
+fun albumArtPNGExists(trackData: TrackData): Boolean =
+    trackData.flacAlbumPathAbsolute.toPath().resolve(Config.albumArtFile).exists()
 
 @OptIn(ExperimentalPathApi::class)
 fun isAlbumArtUpdated(trackData: TrackData): Boolean =
